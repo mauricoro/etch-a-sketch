c442144c2240a70e36b16ae793a16ae9b57189ff
mauricoro <119470826+mauricoro@users.noreply.github.com>
2023-07-09 22:52:07 -0500

    Add changing square amount

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,19 +1,22 @@
 function addColor(e) {
     this.classList.add('color');
+    console.log(e);
 }
 
+let scale = 14;
 
 
 const container = document.querySelector(".container");
 
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < scale; i++){
     let row = document.createElement('div');
-    row.classList.add('row');
-    for(let j = 0; j < 16; j++){
+    row.classList.add('row');   
+    for(let j = 0; j < scale; j++){
         let div = document.createElement('div');
         div.addEventListener('mouseover', addColor);
         div.classList.add('square');
+        div.style.minHeight = (90 / scale - (0.2 - 1 / scale)) + "vh"; //NEED TO SUBTRACT BORDERS HEIGHT
         row.append(div);
     }
     container.appendChild(row);
