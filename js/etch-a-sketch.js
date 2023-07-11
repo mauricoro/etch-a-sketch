3f77fd9038ead1e5060ad707f8006a106542eb5f
mauricoro <119470826+mauricoro@users.noreply.github.com>
2023-07-11 17:41:32 -0500

    Add button and changing squares

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -3,25 +3,50 @@ function addColor(e) {
     console.log(e);
 }
 
-let scale = 14;
+function addGrid(scale){
+    let container = document.querySelector(".container");
+    let button = document.createElement('button');
+    button.onclick = updateGrid;
+    button.textContent = "New Grid"
+    container.appendChild(button);
 
-
-const container = document.querySelector(".container");
-
-
-for (let i = 0; i < scale; i++){
-    let row = document.createElement('div');
-    row.classList.add('row');   
-    for(let j = 0; j < scale; j++){
-        let div = document.createElement('div');
-        div.addEventListener('mouseover', addColor);
-        div.classList.add('square');
-        div.style.minHeight = (90 / scale - (0.2 - 1 / scale)) + "vh"; //NEED TO SUBTRACT BORDERS HEIGHT
-        row.append(div);
+    for (let i = 0; i < scale; i++){
+        let row = document.createElement('div');    
+        row.classList.add('row');   
+        for(let j = 0; j < scale; j++){
+            let square = document.createElement('div');
+            square.addEventListener('mouseover', addColor);
+            square.classList.add('square');
+            square.style.minHeight = (90 / scale - (0.2 - 1 / scale)) + "vh"; //NEED TO SUBTRACT BORDERS HEIGHT
+            row.append(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
 
-const squares = document.querySelectorAll('.square');
-squares
+function removeGrid(){
+    let container = document.querySelector(".container")
+    body.removeChild(container);    
+}
+
+
+function updateGrid(){
+    let number = Number(prompt("Squares?"));
+    console.log(number);
+    if(number && number > 0 && number <= 100){
+        removeGrid();
+        let container = document.createElement('div');
+        container.classList.add('container');
+        body.appendChild(container);
+        addGrid(number);
+    }
+    
+}
+
+let body = document.querySelector('body');
+let scale = 16;
+addGrid(scale);
+ 
+
+
 
