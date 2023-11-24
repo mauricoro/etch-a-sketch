a9cfb703d54a1d71b7e3cfc0e9ceb15bf492b7e3
mauricoro <119470826+mauricoro@users.noreply.github.com>
2023-11-24 11:35:25 -0600

    Add drawing, square size, and color

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,52 +1,69 @@
 function addColor(e) {
-    this.classList.add('color');
-    console.log(e);
-}
+  // original
+  // this.classList.add('color');
 
-function addGrid(scale){
-    let container = document.querySelector(".container");
-    let button = document.createElement('button');
-    button.onclick = updateGrid;
-    button.textContent = "New Grid"
-    container.appendChild(button);
+  // bonus 1
+  this.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)}`
 
-    for (let i = 0; i < scale; i++){
-        let row = document.createElement('div');    
-        row.classList.add('row');   
-        for(let j = 0; j < scale; j++){
-            let square = document.createElement('div');
-            square.addEventListener('mouseover', addColor);
-            square.classList.add('square');
-            square.style.minHeight = (90 / scale - (0.2 - 1 / scale)) + "vh"; //NEED TO SUBTRACT BORDERS HEIGHT
-            row.append(square);
-        }
-        container.appendChild(row);
+  // bonus 2
+  /*
+    let squareColor = this.style.backgroundColor
+    let endIndex = 7;
+    while(!Number(squareColor.substring(4,endIndex))){
+        endIndex -= 1;
     }
+
+    let rgb = Number(squareColor.substring(4,endIndex));
+
+    if(rgb > 5){
+        rgb = rgb - 25;
+        this.style.backgroundColor = `rgb(${rgb},${rgb},${rgb})`
+        console.log(this.style.backgroundColor);
+
+    } */
 }
 
-function removeGrid(){
-    let container = document.querySelector(".container")
-    body.removeChild(container);    
-}
+function addGrid(scale) {
+  const container = document.querySelector(".container")
+  const button = document.createElement("button")
+  button.onclick = updateGrid
+  button.textContent = "New Grid"
+  container.appendChild(button)
 
-
-function updateGrid(){
-    let number = Number(prompt("Squares?"));
-    console.log(number);
-    if(number && number > 0 && number <= 100){
-        removeGrid();
-        let container = document.createElement('div');
-        container.classList.add('container');
-        body.appendChild(container);
-        addGrid(number);
+  for (let i = 0; i < scale; i++) {
+    const row = document.createElement("div")
+    row.classList.add("row")
+    for (let j = 0; j < scale; j++) {
+      const square = document.createElement("div")
+      square.classList.add("square")
+      square.addEventListener("mouseover", addColor)
+      // square.style.backgroundColor = "rgb(255,255,255)";  //For bonus 2
+      square.style.minHeight = `${90 / scale - (0.2 - 1 / scale)}vh` // NEED TO SUBTRACT BORDERS HEIGHT
+      row.append(square)
     }
-    
+    container.appendChild(row)
+  }
 }
 
-let body = document.querySelector('body');
-let scale = 16;
-addGrid(scale);
- 
+function removeGrid() {
+  const container = document.querySelector(".container")
+  body.removeChild(container)
+}
 
+function updateGrid() {
+  const number = Number(prompt("Squares?"))
+  console.log(number)
+  if (number && number > 0 && number <= 100) {
+    removeGrid()
+    const container = document.createElement("div")
+    container.classList.add("container")
+    body.appendChild(container)
+    addGrid(number)
+  }
+}
 
-
+const body = document.querySelector("body")
+const scale = 16
+addGrid(scale)
